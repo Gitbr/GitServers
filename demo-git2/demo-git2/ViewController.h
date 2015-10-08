42debbfe108990aa177b26e48bd0f86019ae577e
15917715974 <mydehp@live.com>
2015-10-08 16:12:34 +0800

    Justfortest

---
+++ demo-git2/demo-git2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  demo-git2
+//
+//  Created by tarena04 on 15/10/8.
+//  Copyright (c) 2015年 tarena04. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ demo-git2/demo-git2/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  demo-git2
+//
+//  Created by tarena04 on 15/10/8.
+//  Copyright (c) 2015年 tarena04. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
